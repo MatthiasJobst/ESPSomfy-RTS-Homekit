640d0c05045b273542c1ff5167d390f58e0f68a6
Matthias Jobst <matthias.jobst@gmx.net>
2026-04-14 20:44:55 +0200

    Bump version to v0.2.31

--- data/settings.js
+++ data/settings.js
@@ -106,7 +106,7 @@ var security = new Security();
 
 class General {
     initialized = false; 
-    appVersion = 'v0.2.3';
+    appVersion = 'v0.2.31';
     reloadApp = false;
     init() {
         if (this.initialized) return;
