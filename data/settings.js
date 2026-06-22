9efb129f1d14610c966d22f371e38eea0331d48f
Matthias Jobst <matthias.jobst@gmx.net>
2026-06-22 18:43:00 +0200

    Bump version to v0.7.1

--- data/settings.js
+++ data/settings.js
@@ -106,7 +106,7 @@ var security = new Security();
 
 class General {
     initialized = false; 
-    appVersion = 'v0.7.0';
+    appVersion = 'v0.7.1';
     reloadApp = false;
     init() {
         if (this.initialized) return;
